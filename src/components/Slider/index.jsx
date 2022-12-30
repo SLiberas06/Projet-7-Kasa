2cc8fce9f71cd6d660119222179230fba00e8198
Sabrina Liberas <sabrina.liberas@hotmail.fr>
2022-12-30 15:07:57 +0100

    Slider deuxieme format de test

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,30 +1,82 @@
-import React from 'react'
-// import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
-import 'react-slideshow-image/dist/styles.css'
-import { Slide } from 'react-slideshow-image'
-import DataProperty from '../../datas/logements.json'
-import styled from 'styled-components'
+import { useCallback, useEffect, useRef, useState } from 'react'
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
 
-const StyledSlider = styled(Slide)`
-  width: 10vw;
-  height: 20vh;
-  // background-color: red;
-`
-const ImgSlider = styled.img`
-  width: 100%;
-`
+const ContainerSlider = {
+  width: '80vw',
+  height: '40vh',
+  backgroundColor: 'red',
+  diplay: 'flex',
+  flexDirection: 'row',
+}
 
-const SlideShow = () => {
-  // const SlideProperty = {}
+const StyledSlider = {
+  width: '40vw',
+  height: '20vh',
+  backgroundColor: 'blue',
+}
+
+const rightArrowStyles = {
+  position: 'absolute',
+  top: '30%',
+  transform: 'translate(0, -50%)',
+  right: '32px',
+  fontSize: '45px',
+  color: '#fff',
+  zIndex: 1,
+  cursor: 'pointer',
+}
+
+const leftArrowStyles = {
+  position: 'absolute',
+  top: '30%',
+  transform: 'translate(0, -50%)',
+  left: '32px',
+  fontSize: '45px',
+  color: '#fff',
+  zIndex: 1,
+  cursor: 'pointer',
+}
+
+const SlideShow = ({ pictures }) => {
+  const timer = useRef(null)
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  const getPrevious = () => {
+    const firstPicture = currentIndex === 0
+    const newIndex = firstPicture ? pictures.length - 1 : currentIndex - 1
+    setCurrentIndex(newIndex)
+  }
+  const getNext = useCallback(() => {
+    const lastPicture = currentIndex === pictures.length - 1
+    const newIndex = lastPicture ? 0 : currentIndex + 1
+    setCurrentIndex(newIndex)
+  }, [currentIndex, pictures])
+
+  const getSlide = (pictureIndex) => {
+    setCurrentIndex(pictureIndex)
+  }
+
+  useEffect(() => {
+    if (timer.current) {
+      clearTimeout(timer.current)
+    }
+    timer.current = setTimeout(() => {
+      getNext()
+    }, 2000)
+  })
   return (
-    <div>
-      <StyledSlider>
-        {DataProperty.map((property, index) => (
-          <div key={index.toString()} id={property.id}>
-            <ImgSlider src={property} alt={property.title} />
-          </div>
-        ))}
-      </StyledSlider>
+    <div style={ContainerSlider}>
+      <div style={StyledSlider}>
+        <div onClick={getPrevious}>
+          <FaChevronLeft style={leftArrowStyles} />
+        </div>
+        <div onClick={getNext}>
+          <FaChevronRight style={rightArrowStyles} />
+        </div>
+        <div>slide1</div>
+        <div>slide2</div>
+        <div>slide3</div>
+      </div>
     </div>
   )
 }
